8d8944d05cda1abd767d78cb46e6e4847baa5ee9
nicholas-arocho <n_arocho@apple.com>
2022-09-12 14:26:59 -0700

    connect a component to the Redux store

--- src/todos/NewTodoForm.js
+++ src/todos/NewTodoForm.js
@@ -1,7 +1,11 @@
 import React, { useState } from "react";
+import { connect } from "react-redux";
+import { createTodo } from "actions";
 import "./NewTodoForm.css";
 
-const NewTodoForm = () => {
+// `todos` prop comes from the `mapStateToProps` method below
+// `onCreatePressed` comes from `mapDispatchToProps`
+const NewTodoForm = ({ todos, onCreatePressed }) => {
   const [inputValue, setInputValue] = useState("");
 
   return (
@@ -13,9 +17,35 @@ const NewTodoForm = () => {
         value={inputValue}
         onChange={(e) => setInputValue(e.target.value)}
       />
-      <button className="new-todo-button">Create Todo</button>
+      <button
+        onClick={() => {
+          const isDuplicateText = todos.some(
+            (todo) => todo.text === inputValue
+          );
+          if (!isDuplicateText) {
+            onCreatePressed(inputValue);
+            setInputValue("");
+          }
+        }}
+        className="new-todo-button"
+      >
+        Create Todo
+      </button>
     </div>
   );
 };
 
-export default NewTodoForm;
+// `state` here represents the ENTIRE Redux state
+// the job of `mapStateToProps` is to take the `state` object, and returning
+// another object containing the pieces of that state that this component needs access to
+const mapStateToProps = (state) => ({
+  todos: state.todos,
+});
+
+// similar to `mapStateToProps`, but instead of taking `state`, as an arg - it takes `dispatch`
+// `dispatch` allows our components to trigger actions which our store will respond to
+const mapDispatchToProps = (dispatch) => ({
+  onCreatePressed: (text) => dispatch(createTodo(text)),
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(NewTodoForm);
