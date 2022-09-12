102c667c55d43af06e85122e9ce8dea1367b33d4
nicholas-arocho <n_arocho@apple.com>
2022-09-12 15:15:07 -0700

    add redux-persist to store changes in local storage

--- src/index.js
+++ src/index.js
@@ -1,14 +1,21 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.js";
+import { persistStore } from "redux-persist";
+import { PersistGate } from "redux-persist/lib/integration/react";
 import { Provider } from "react-redux";
 import { configureStore } from "./store";
 
 const container = document.getElementById("app");
 const root = createRoot(container);
 
+const store = configureStore();
+const persistor = persistStore(store);
+
 root.render(
-  <Provider store={configureStore()}>
-    <App />
+  <Provider store={store}>
+    <PersistGate loading={<div>Loading...</div>} persistor={persistor}>
+      <App />
+    </PersistGate>
   </Provider>
 );
--- src/store.js
+++ src/store.js
@@ -1,10 +1,20 @@
 import { createStore, combineReducers } from "redux";
+import { persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage";
+import autoMergeLevel2 from "redux-persist/lib/stateReconciler/autoMergeLevel2";
 import { todos } from "./todos/reducers";
 
 const reducers = {
   todos,
 };
 
-const rootReducer = combineReducers(reducers);
+const persistConfig = {
+  key: "root",
+  storage, // defaults to `localStorage` on the web
+  stateReconciler: autoMergeLevel2, // tells ReduxPersist how to store the inital & stored states of the app
+};
 
-export const configureStore = () => createStore(rootReducer);
+const rootReducer = combineReducers(reducers);
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+export const configureStore = () => createStore(persistedReducer);
